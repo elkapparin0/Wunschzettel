2be5a7cc64363c4a738e4205de2cf14dcc6bcd6d
elkapparin0 <jonaspetter04@gmail.com>
2026-08-25 01:56:26 +0200

    Extra Hinweise

--- startseite.js
+++ startseite.js
@@ -1,3 +1,6 @@
 document.getElementById("btnStart").addEventListener("click", () => {
        window.location.href = "wunschzettel.html"; 
 });
+document.getElementById("btnHinweise").addEventListener("click", () => {
+       window.location.href = "hinweise.html";
+});
